d0712ab204fedf4f111441c0dff6dd5246d503bf
John Nunemaker <nunemaker@gmail.com>
2016-06-12 20:24:00 -0400

    Terrible hack to know if value is a set
    Instead, maybe I'll encode each data type in the value or something long term rather than just set, but I'm just seeing how this kv thing feels so I'm fine for now.

--- lib/flipper/adapter.rb
+++ lib/flipper/adapter.rb
@@ -1,6 +1,8 @@
 module Flipper
   module Adapter
     SET_SEPARATOR = ",".freeze
+    SET_PREFIX = "flipper_set:".freeze
+    SET_VALUE_REGEXP = /^#{SET_PREFIX}/
     SET_SERIALIZER = lambda { |object| object.to_a.join(SET_SEPARATOR) }
     SET_DESERIALIZER = lambda { |raw| raw.to_s.split(SET_SEPARATOR).to_set }
 
@@ -9,7 +11,11 @@ module Flipper
     def mget(keys)
       hash = {}
       keys.each do |key|
-        hash[key] = get(key)
+        value = get(key)
+        if value =~ SET_VALUE_REGEXP
+          value = sload(value)
+        end
+        hash[key] = value
       end
       hash
     end
@@ -71,13 +77,13 @@ module Flipper
     # Public: Override with data store specific implementation that is
     # more efficient/transactional.
     def sdump(object)
-      SET_SERIALIZER.call(object)
+      SET_PREFIX + SET_SERIALIZER.call(object)
     end
 
     # Public: Override with data store specific implementation that is
     # more efficient/transactional.
     def sload(object)
-      SET_DESERIALIZER.call(object)
+      SET_DESERIALIZER.call(object.to_s.gsub(SET_VALUE_REGEXP, ""))
     end
   end
 end
